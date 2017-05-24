a7460cc1869a11ecdc4f0da5be1a5fc1823708a4
Ubuntu <ubuntu@dareon.novalocal>
2017-05-24 08:26:02 +0000

    Testing smoketest

--- smoketest.sh
+++ smoketest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Basic if statement
 
-if curl -I  --stderr /dev/null http://localhost | head -1 | cut -d' ' -f2 | grep "200" > /dev/null; then 
+if curl -I  --stderr /dev/null http://localhost/login | head -1 | cut -d' ' -f2 | grep "200" > /dev/null; then 
    exit 0
 else
    exit 1
